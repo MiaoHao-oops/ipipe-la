02379a907d81240433585d6d14d9f785cdf95629
MiaoHao-oops <haomiao19@mails.ucas.ac.cn>
2023-02-20 16:02:10 +0800

    remove useless header include

--- arch/loongarch/kernel/time.c
+++ arch/loongarch/kernel/time.c
@@ -9,7 +9,7 @@
  * Free Software Foundation;  either version 2 of the  License, or (at your
  * option) any later version.
  */
-#include "linux/ipipe_lock.h"
+
 #include <linux/clockchips.h>
 #include <linux/export.h>
 #include <linux/init.h>
